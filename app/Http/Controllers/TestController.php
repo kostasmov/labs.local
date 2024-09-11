2cae8c015b3aabdbdb427bd1fd88da2e7b29b051
kostasmov <kostasmov@mail.ru>
2024-09-11 11:08:48 +0300

    test validation with DB

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests\TestValidationRequest;
 use App\Http\Requests\TestVerificationRequest;
+
+use App\Models\TestResult;
+use Illuminate\Support\Facades\Validator;
 
 class TestController extends Controller
 {
     public function submit(TestValidationRequest $request)
     {
+        TestResult::create([
+            'full_name' => $request->input('full_name'),
+            'course' => $request->input('course'),
+
+            'quest1' => (int) $request->input('quest1'),
+            'quest2' => (int) $request->input('quest2'),
+            'quest3' => (int) $request->input('quest3'),
+
+            'correct1' => $request->input('quest1') === '2',
+            'correct2' => $request->input('quest1') === '3',
+            'correct3' => $request->input('quest1') === 'триггер'
+        ]);
+
         app(TestVerificationRequest::class);
 
         return redirect()->back()->withInput()->with('success', 'Тест успешно пройден!');
